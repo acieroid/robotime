aebdff2c8c64d0594e5e0303cc9bcb2007287ebf
acieroid <none@none>
2010-07-10 13:07:59 +0200

    adding player movements

--- engine.lisp
+++ engine.lisp
@@ -1,6 +1,14 @@
 (defparameter *font-size* 14)
-(defparameter *keys*
-  '((quit . :escape))
+(defparameter *keys*                    ; TODO: add qwerty bindings
+  '((quit . :escape)
+    (left . (:left :a))
+    (right . (:right :i))
+    (up . (:up :eacute))
+    (down . (:down :y))
+    (upleft . :b)
+    (upright . :p)
+    (downleft . :agrave)
+    (downright . :x))
   "The actions with the associated keybindings")
 
 (defvar *actual-time* 0 "The actual time of the game")
@@ -16,7 +24,9 @@
 (defparameter *engine* (make-instance 'robotime))  ; for debugging purposes only
 
 (defun run ()
-  (uid:run *engine*))
+  (handler-bind ((babel-encodings:invalid-utf8-starter-byte
+                  (lambda (c) (invoke-restart 'continue))))
+    (uid:run *engine*)))
 
 (defmethod uid:init ((game robotime))
   (setf uid:*font* (make-instance 'uid::ftgl-font
@@ -29,10 +39,29 @@
   (draw (player game)))
 
 (defmacro defkey (action &body body)
-  `(defmethod uid:on-key-down ((game robotime) keycode
-                               (keysym (eql (cdr (assoc ',action *keys*))))
-                               string)
-     ,@body))
+  (let* ((keys (cdr (assoc action *keys*)))
+         (keysyms
+          (etypecase keys
+            (keyword (cons keys nil))
+            (list keys))))
+    (cons 'progn
+          (loop for keysym in keysyms
+             collect
+             `(defmethod uid:on-key-down ((game robotime) keycode
+                                          (keysym (eql ,keysym))
+                                          string)
+                ,@body)))))
 
 (defkey quit
   (uid:close-window game))
+
+(defmacro defdirections (keys directions)
+  (cons 'progn
+        (loop for key in keys
+           for dir in directions
+           collect `(defkey ,key
+                      (move (player game) ,dir t)))))
+
+(defdirections
+    (up down left right upright upleft downright downleft)
+    (:north :south :west :east :north-east :north-west :south-east :south-west))
--- entity.lisp
+++ entity.lisp
@@ -3,6 +3,28 @@
    (time-died :accessor time-died :initform -1)
    (positions :accessor positions :initform '())))
 
+(defgeneric move (entity direction forwardp)
+  (:documentation "Move the entity in a direction, this should also
+  handle all the consequences of this movement (crash etc.). FORWARDP
+  is T if the time is moving forward, NIL if moving backward. DIRECTION
+  is a keyword indicating a cardinal direction (eg. :NORTH,
+  :NORTH-EAST etc.)"))
+
+(defmethod move :after ((entity entity) direction forwardp)
+  (destructuring-bind (x y)
+      (case direction
+        (:north '(0 1))
+        (:south '(0 -1))
+        (:east '(1 0))
+        (:west '(-1 0))
+        (:north-east '(1 1))
+        (:north-west '(-1 1))
+        (:south-east '(1 -1))
+        (:south-west '(-1 -1))
+        (otherwise (error "Not a valid direction: ~a" direction)))
+    (setf (x entity) (+ (x entity) x))
+    (setf (y entity) (+ (y entity) y))))
+
 (defclass player (entity)
   ())
 
@@ -16,3 +38,6 @@
   (draw-rectangle-in-case (x player) (y player)
                           (/ *case-size* 2)
                           :color uid:*red*))
+
+(defmethod move ((player player) direction forwardp)
+  (declare (ignore forwardp)))
